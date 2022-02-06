c4d8894eafa411eccdf3b0968675fcb2bf2af60c
fnovoas <fnovoas@unal.edu.co>
2022-02-06 18:20:44 -0500

    Restauración 2 de "Eliminado paquete innecesario"
    Se volvió a restaurar esa versión del proyecto.

--- UNALChess/src/board/movimiFichas/MovRey.java
+++ UNALChess/src/board/movimiFichas/MovRey.java
@@ -122,7 +122,8 @@ public class MovRey extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
+        Con_cambio cambio= new Con_cambio();
+        cambio.setVisible(true);
         
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
